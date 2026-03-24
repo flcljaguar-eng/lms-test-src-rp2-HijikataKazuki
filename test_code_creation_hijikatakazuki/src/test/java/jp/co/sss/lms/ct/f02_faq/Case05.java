2c30fd6608f0d924ced13b8cd86875d0975d3d01
k-hijikata <flcljaguar@gmail.com>
2026-03-24 16:47:48 +0900

    fix:Case01 Case02の再レビュー指摘事項の修正
    プロジェクト直下、docフォルダ内の.gitignoreを削除
    テスト仕様書のハイパーリンク削除

--- test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import jp.co.sss.lms.ct.util.TestUrlUtil;
 
 /**
  * 結合テスト よくある質問機能
@@ -38,36 +40,101 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo(TestUrlUtil.TOP_PAGE);
+
+		final String title = webDriver.getTitle();
+		final WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+		String button = loginButton.getAttribute("value");
+
+		assertEquals("ログイン | LMS", title);
+		assertEquals("ログイン", button);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// WebElementを利用してログイン処理を行う
+		final WebElement userId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+		final WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+
+		userId.clear();
+		userId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("StudentAA01");
+		loginButton.click();
+
+		// タイトルがコース詳細画面になっていることを確認する
+		final String title = webDriver.getTitle();
+
+		assertEquals("コース詳細 | LMS", title);
+
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// WebElementを利用してログイン処理を行う
+		final WebElement userId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+		final WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+
+		userId.clear();
+		userId.sendKeys("StudentAA01");
+		password.clear();
+		password.sendKeys("StudentAA01");
+		loginButton.click();
+
+		// タイトルがコース詳細画面になっていることを確認する
+		final String title = webDriver.getTitle();
+
+		assertEquals("コース詳細 | LMS", title);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		final WebElement faq = webDriver.findElement(By.linkText("よくある質問"));
+		faq.click();
+
+		// 新規タブが生成されていることの確認と操作タブの切り替え
+		Object[] allTab = webDriver.getWindowHandles().toArray();
+		assertEquals(allTab.length, 2);
+		webDriver.switchTo().window((String) allTab[1]);
+
+		final WebElement faqPage = webDriver.findElement(By.tagName("h2"));
+		assertEquals("よくある質問", faqPage.getText());
+
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		// WebElementを利用し、テキストフォームから検索を行う
+		final WebElement searchForm = webDriver.findElement(By.id("form"));
+		final WebElement searchButton = webDriver.findElement(By.xpath(TestUrlUtil.SEARCH_VALUE));
+
+		searchForm.clear();
+		searchForm.sendKeys("IT");
+		searchButton.click();
+
+		final WebElement grant = webDriver.findElement(By.className(TestUrlUtil.GRANT_VALUE));
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
--- test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/util/TestUrlUtil.java
+++ test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/util/TestUrlUtil.java
@@ -5,4 +5,8 @@ public interface TestUrlUtil {
 	public static final String TOP_PAGE = "http://localhost:8080/lms";
 
 	public static final String HELP_XPATH = "/html/body/div[1]/nav/div/div[2]/ul[1]/li[4]/ul/li[4]/a";
+
+	public static final String SEARCH_VALUE = "/html/body/div[1]/div/div[1]/div/div[1]/form/fieldset/div[2]/div/input[1]";
+
+	public static final String GRANT_VALUE = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[2]/div/table/tbody/tr[4]/td/dl/dt/span[2]";
 }
