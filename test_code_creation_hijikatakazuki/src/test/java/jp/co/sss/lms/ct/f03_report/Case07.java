d14df15800055dc7ff718d7057a10b6040d615f5
k-hijikata <flcljaguar@gmail.com>
2026-03-26 15:49:52 +0900

    add:Case07.javaレポートが正常に保存されることを確認するクラスの追加
    以下検査内容を含むCase07.javaの新規作成
    ・未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移
    ・「提出する」ボタンを押下しレポート登録画面に遷移
    ・報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される

--- test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,10 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +13,14 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.TestStringUtil;
+import jp.co.sss.lms.ct.util.TestUrlUtil;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +47,110 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo(TestUrlUtil.TOP_PAGE);
+
+		final String title = webDriver.getTitle();
+		final WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+		String button = loginButton.getAttribute("value");
+
+		assertEquals("ログイン | LMS", title);
+		assertEquals("ログイン", button);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// WebElementを利用してログイン処理を行う
+		final WebElement userId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+		final WebElement loginButton = webDriver.findElement(By.className("btn-primary"));
+
+		userId.clear();
+		userId.sendKeys(TestStringUtil.STUDENT_PASSWORD);
+		password.clear();
+		password.sendKeys(TestStringUtil.STUDENT_PASSWORD);
+		loginButton.click();
+
+		// タイトルがコース詳細画面になっていることを確認する
+		final String title = webDriver.getTitle();
+
+		assertEquals("コース詳細 | LMS", title);
+
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// スケジュールをリストとして取得し未提出ステータスの日付を抽出
+		final List<WebElement> dateTable = webDriver.findElements(By.cssSelector("table.sctionList tr"));
+		WebElement notSubmit = null;
+		for (WebElement dateElement : dateTable) {
+			if (dateElement.getText().contains("未提出")) {
+				notSubmit = dateElement;
+				break;
+			}
+		}
+		((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(false);", notSubmit);
+		((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0, 200);");
+		WebElement detailButton = notSubmit.findElement(By.className("btn"));
+		detailButton.click();
+
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("panel")));
+
+		final String sectionDetail = webDriver.getTitle();
+		assertEquals(TestStringUtil.SECTION_DETAIL_PAGE, sectionDetail);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		final WebElement toSubmitBtn = webDriver
+				.findElement(By.xpath("//input[@type='submit' and @value='日報【デモ】を提出する']"));
+		((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView(false);", toSubmitBtn);
+		final WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("panel")));
+		toSubmitBtn.click();
+
+		final String submitPage = webDriver.getTitle();
+		assertEquals(TestStringUtil.SUBMIT_REPORT_PAGE, submitPage);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		final WebElement reportForm = webDriver.findElement(By.className("form-control"));
+		final WebElement submitBtn = webDriver.findElement(By.className("btn-primary"));
+
+		reportForm.clear();
+		reportForm.sendKeys(TestStringUtil.TEST_TEXT);
+		getEvidence(new Object() {
+		}, "1");
+
+		submitBtn.click();
+
+		final WebElement submitedReport = webDriver
+				.findElement(By.xpath("//input[@type='submit' and @value='提出済み日報【デモ】を確認する']"));
+
+		assertNotNull(submitedReport);
+		getEvidence(new Object() {
+		}, "2");
 	}
 
 }
--- test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/util/TestStringUtil.java
+++ test_code_creation_hijikatakazuki/src/test/java/jp/co/sss/lms/ct/util/TestStringUtil.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.util;
 
 public interface TestStringUtil {
+	public static final String SECTION_DETAIL_PAGE = "セクション詳細 | LMS";
+
+	public static final String SUBMIT_REPORT_PAGE = "レポート登録 | LMS";
 	public static final String STUDENT_PASSWORD = "StudentAA01";
 	public static final String APPLY_FOR_TRAINING_QUESTION = "Q.助成金書類の作成方法が分かりません";
 
@@ -11,4 +14,5 @@ public interface TestStringUtil {
 	public static final String CANCELL_FEE_AND_DROP_SCHOOL_ANSER = "A. 受講者の退職や解雇等、やむを得ない事情による途中終了に関してなど、"
 			+ "事情をお伺いした上で、協議という形を取らせて頂きます。 弊社営業担当までご相談下さい。";
 
+	public static final String TEST_TEXT = "テスト用テキスト";
 }
